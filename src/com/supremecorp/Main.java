0b2e018a7478057b6ec727d70f065e47f8289eb3
Suprememajor <ndipnobertetta@gmail.com>
2021-09-12 01:14:50 +0100

    Solved Pythagorean triplet problem. Closes #1

--- src/com/supremecorp/Main.java
+++ src/com/supremecorp/Main.java
@@ -1,8 +1,18 @@
 package com.supremecorp;
 
+import com.supremecorp.week0.PythagoreanTriple;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(PythagoreanTriple.pythagoreanTriple(24));
+        System.out.println(PythagoreanTriple.pythagoreanTriple(10));
+        System.out.println(PythagoreanTriple.pythagoreanTriple(306));
+        System.out.println(PythagoreanTriple.pythagoreanTriple(10000));
+        System.out.println(PythagoreanTriple.pythagoreanTriple(100000));
+        System.out.println(PythagoreanTriple.pythagoreanTriple(1000000));
+        System.out.println(PythagoreanTriple.pythagoreanTriple(10000000));
+
     }
 }
---
+++ src/com/supremecorp/week0/PythagoreanTriple.java
@@ -0,0 +1,20 @@
+package com.supremecorp.week0;
+
+/**
+ * Created by suprememajor on the 9/11/21
+ */
+public class PythagoreanTriple {
+    public static String pythagoreanTriple(int input) {
+        long startTime = System.currentTimeMillis();
+        int hyp;
+        for (int i = 1; i < input/2; i++)
+            for (int j = i + 1; j < input/2; j++) {
+                hyp = input - (i + j);
+                if ( ( i * i + j * j == hyp * hyp) && (j != hyp) && (i != j)) {
+                    return "" + input + ": " + i + " " + j + " " + hyp;
+                }
+
+            }
+        return input +": None";
+    }
+}
